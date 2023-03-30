c379072841c29be8a85a14f72a677f67691357fa
arjun-1510 <Arjung1510@gmail.com>
2023-03-30 12:43:36 +0530

    changes

---
+++ src/App.java
@@ -0,0 +1,11 @@
+ class App {
+
+    static String a;
+    static char b;
+    public static void main(String[] args) throws Exception {
+       a="Arjun is good progrom....";
+       System.out.println(a);
+       b=3;
+       System.out.println(b);
+    }
+}
